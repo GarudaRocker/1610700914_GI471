7f081aea2503a1e36bc83fbf45fd381dcb492dba
DESKTOP-LDR5M97\Win 10 Home <anucha.sens@bumail.net>
2020-09-05 17:33:13 +0700

    Revert "การบ้าน week1 เหลือ coding"
    This reverts commit 6a54f124f4c02d129735c8f639c553671dfce5c4.

--- GI471/Assets/Week01/Scripts/Queue.cs
+++ GI471/Assets/Week01/Scripts/Queue.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class Queue : MonoBehaviour
 {
-    public void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        List<string> FruitList = new List<string>();
-        FruitList.Add("Apple");
-        FruitList.Add("Orange");
-        FruitList.Add("Carrot");
-        FruitList.Add("Strawberry");
-        FruitList.Add("Banana");
-
-        print(FruitList);
+        
     }
 
-    //public void Update()
-    //{
-    //    List<string> FruitList = new List<string>();
-    //    //string[] fruit = { "Apple", "Orange", "Carrot", "Strawberry", "Banana" };
-    //    string i =  ;
-    //    while (i < FruitList)
-    //    {
-    //        if (Input.GetKeyDown(KeyCode.Return))
-    //        {
-    //            FruitList.RemoveAt(0);
-    //            print(FruitList);
-    //        }
-    //        else
-    //        {
-    //            print("Item is Empty");
-    //        }
-    //        i++;
-    //    }
-    //    }
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
